fa8a0571147a1bdc7fb85aa14d03bd60e9372329
horz.d <he1236@ajou.ac.kr>
2022-08-10 18:15:14 +0900

    client/core - view 기본구성 완료

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,15 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
-
-import Header from './common/components/Header';
-import Footer from './common/components/Footer';
-import Home from './pages/Home/Home.js';
+import Header from "./common/components/Header";
+import Footer from "./common/components/Footer";
+import Home from "./pages/Home/Home.js";
 import History from "./pages/History/History";
-
+import Core from "./pages/Core/Core";
 
 ////리덕스 도입 시 활성화
 // import { Provider } from "react-redux";
 // import Store from "./app/Store";
-
 
 //social로그인
 import KakaoCallBack from "./pages/user/kakao/KakaoCallBack";
@@ -19,18 +17,18 @@ import SocialSignUp from "./pages/user/SocialSignUp";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       {/* <Provider store={Store}> */}
       <Header />
       <Routes>
-      <Route path='/' element={<Home />} />
+        <Route path="/" element={<Home />} />
 
-
-        <Route path="/history" element={<History/>} />
+        <Route path="/core" element={<Core />} />
+        <Route path="/history" element={<History />} />
       </Routes>
       <Routes path="oauth">
-            <Route path="kakao/callback" element={<KakaoCallBack />} />
-            <Route path="signUp" element={<SocialSignUp />} />
+        <Route path="kakao/callback" element={<KakaoCallBack />} />
+        <Route path="signUp" element={<SocialSignUp />} />
       </Routes>
 
       <Footer />
