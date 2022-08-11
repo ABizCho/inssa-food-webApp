a25510e52f459af2989af9c41413e5c59a9985eb
horz.d <he1236@ajou.ac.kr>
2022-08-11 12:48:39 +0900

    Header부분 라우팅 권한관리 대체할 cookie 야매관리 적용-향후 삭제 요망

--- client/src/common/components/Header.js
+++ client/src/common/components/Header.js
@@ -1,70 +1,101 @@
-import './Header.css';
-import lflogo from './lflogo.jpg';
-import kakaLoginButtonImg from '../../img/kakao_login_medium.png';
-import { useNavigate } from 'react-router-dom';
+import "./Header.css";
+import lflogo from "./lflogo.jpg";
+import kakaLoginButtonImg from "../../img/kakao_login_medium.png";
+
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const Header = () => {
-  const REST_API_KEY = '0abf97780f442400eccc7cd004baabab';
-  const REDIRECT_URI = 'http://localhost:3000/oauth/kakao/callback';
+  const REST_API_KEY = "0abf97780f442400eccc7cd004baabab";
+  const REDIRECT_URI = "http://localhost:3000/oauth/kakao/callback";
 
+  // 야매:라우팅 권한관리로 개선되어야할 로직---------------
+  const [cookies, setCookie, removeCookie] = useCookies(["userData"]);
   const navigate = useNavigate();
 
+  useEffect(() => {
+    if (cookies.userData === undefined) {
+      navigate("/");
+    }
+  }, [cookies]);
+  // -----------------------------------------
+
+  // ------------------kakao Oauth-------------------
   //1번
   const KAKAO_AUTH_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   const onLoginClick = () => {
-    navigate('/login');
+    navigate("/login");
   };
+  //---------------------------------------------------
 
   return (
-    <header className='p-3 text-bg-dark header-container'>
+    <header className="p-3 text-bg-dark header-container">
       <div>
-        <img src={lflogo} alt='logo' style={{ height: '200px', width: '200px' }} />
+        <img
+          src={lflogo}
+          alt="logo"
+          style={{ height: "200px", width: "200px" }}
+        />
       </div>
-      <div className='container'>
-        <div className='d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start'>
-          <a href='/' className='d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none'>
-            <svg className='bi me-2' width='40' height='32' role='img' aria-label='Bootstrap'>
+      <div className="container">
+        <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+          <a
+            href="/"
+            className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"
+          >
+            <svg
+              className="bi me-2"
+              width="40"
+              height="32"
+              role="img"
+              aria-label="Bootstrap"
+            >
               <use></use>
             </svg>
           </a>
 
-          <ul className='nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0'>
+          <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
             <li>
-              <a href='#' className='nav-link px-2 text-white'>
+              <a href="#" className="nav-link px-2 text-white">
                 Home
               </a>
             </li>
             <li>
-              <a href='#' className='nav-link px-2 text-white'>
+              <a href="#" className="nav-link px-2 text-white">
                 About
               </a>
             </li>
             <li>
-              <a href='#' className='nav-link px-2 text-white'>
+              <a href="#" className="nav-link px-2 text-white">
                 Search
               </a>
             </li>
             <li>
-              <a href='#' className='nav-link px-2 text-white'>
+              <a href="#" className="nav-link px-2 text-white">
                 History
               </a>
             </li>
             <li>
-              <a href='#' className='nav-link px-2 text-white'>
+              <a href="#" className="nav-link px-2 text-white">
                 About
               </a>
             </li>
           </ul>
 
-          <div className='text-end'>
+          <div className="text-end">
             <a href={KAKAO_AUTH_URI}>
               <img src={kakaLoginButtonImg} />
             </a>
-            <button type='button' onClick={onLoginClick} className='btn btn-outline-light me-2'>
+            <button
+              type="button"
+              onClick={onLoginClick}
+              className="btn btn-outline-light me-2"
+            >
               Login
             </button>
-            <button type='button' className='btn btn-warning'>
+            <button type="button" className="btn btn-warning">
               Sign-up
             </button>
           </div>
