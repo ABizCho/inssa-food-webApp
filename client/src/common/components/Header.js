10c1f0ffdfd4abdd2a89b879499d9236a9823d45
horz.d <he1236@ajou.ac.kr>
2022-08-12 06:58:06 +0900

    header navigate 완료

--- client/src/common/components/Header.js
+++ client/src/common/components/Header.js
@@ -69,22 +69,36 @@ const Header = () => {
 
           <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
             <li>
-              <a href="#" className="nav-link px-2 text-white">
+              <a
+                href="#"
+                onClick={() => {
+                  navigate("/home");
+                }}
+                className="nav-link px-2 text-white"
+              >
                 Home
               </a>
             </li>
+
             <li>
-              <a href="#" className="nav-link px-2 text-white">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#" className="nav-link px-2 text-white">
+              <a
+                onClick={() => {
+                  navigate("/core");
+                }}
+                href="#"
+                className="nav-link px-2 text-white"
+              >
                 Search
               </a>
             </li>
             <li>
-              <a href="#" className="nav-link px-2 text-white">
+              <a
+                href="#"
+                className="nav-link px-2 text-white"
+                onClick={() => {
+                  navigate("/history");
+                }}
+              >
                 History
               </a>
             </li>
