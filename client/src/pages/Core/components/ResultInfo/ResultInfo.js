23c293b5c1e42e1f7c4a8f14f375177aed739837
horz.d <he1236@ajou.ac.kr>
2022-08-15 16:21:18 +0900

    sound fixed

--- client/src/pages/Core/components/ResultInfo/ResultInfo.js
+++ client/src/pages/Core/components/ResultInfo/ResultInfo.js
@@ -10,8 +10,6 @@ import axios from "axios";
 import urlPort from "./../../../../data/urlPort.json";
 import ReactAudioPlayer from "react-audio-player";
 import ReactPlayer from "react-player";
-import galbi from "../ResultInfo/order_food/1.galbi.mp3";
-
 
 const ResultInfo = () => {
   const navigate = useNavigate();
@@ -23,13 +21,16 @@ const ResultInfo = () => {
   const [foodInfo, setFoodInfo] = useState({});
   const [historyInput, setHistoryInput] = useState({
     title: "",
-    comment: ""
+    comment: "",
   });
 
   //쿠키 사용 준비
 
-  const [cookies, setCookie, removeCookie] = useCookies(["inputImage", "foodInfo",'imgFile']);
-
+  const [cookies, setCookie, removeCookie] = useCookies([
+    "inputImage",
+    "foodInfo",
+    "imgFile",
+  ]);
 
   useEffect(() => {
     console.log("params.id : ", params.id);
@@ -47,17 +48,17 @@ const ResultInfo = () => {
   }, [foodInfo]);
 
   //HistoryInput 변하면 console 찍기
-  useEffect(()=> {
-    console.log("HistoryINPUT : ", historyInput)
-  }, [historyInput])
+  useEffect(() => {
+    console.log("HistoryINPUT : ", historyInput);
+  }, [historyInput]);
 
   const onClickSaveHistory = async () => {
-    
     const historyInfo = {
       ...historyInfoOne,
       title: historyInput.title,
-      comment: historyInput.comment}
-    
+      comment: historyInput.comment,
+    };
+
     await postHistoryData(historyInfo);
     await navigate("/history/list");
   };
@@ -94,14 +95,23 @@ const ResultInfo = () => {
           </div>
           <div className="result-item spicy">spicy: {foodInfo.spicy}</div>
           <div className="result-item caution">caution: {foodInfo.caution}</div>
-          <div className="result-item name_Eng">English Name: {foodInfo.name_Eng}</div>
-          <div className="result-item order_learn_audio">
-            <ReactAudioPlayer src={galbi} autoPlay controls />
+          <div className="result-item name_Eng">
+            English Name: {foodInfo.name_Eng}
           </div>
-          <div className="result-item order_learn_text">🗣️: {foodInfo.order_learn_text}</div>
+          <div className="result-item order_learn_audio">
+            <ReactAudioPlayer src={foodInfo.sound_url} autoPlay controls />
+          </div>
+          <div className="result-item order_learn_text">
+            🗣️: {foodInfo.order_learn_text}
+          </div>
           <div>
             RECIPE
-            <ReactPlayer url={foodInfo.recipie_url} controls width={300} height={300} />
+            <ReactPlayer
+              url={foodInfo.recipie_url}
+              controls
+              width={300}
+              height={300}
+            />
           </div>
 
           <div className="result-item desc">
@@ -109,15 +119,32 @@ const ResultInfo = () => {
             <div className="desc-content">{foodInfo.description}</div>
           </div>
         </div>
-      <div className="history-inputs">
-        <label htmlFor="history-title">Title</label>
-        <input name="history-title" onChange={(e)=> {setHistoryInput( {...historyInput, title: e.target.value} )}} type="text" />
-        <label htmlFor="history-comment">Comment</label>
-        <textarea name="history-comment" onChange={(e)=> {setHistoryInput( {...historyInput, comment: e.target.value} )}} type="text" />
-      </div>
+        <div className="history-inputs">
+          <label htmlFor="history-title">Title</label>
+          <input
+            name="history-title"
+            onChange={(e) => {
+              setHistoryInput({ ...historyInput, title: e.target.value });
+            }}
+            type="text"
+          />
+          <label htmlFor="history-comment">Comment</label>
+          <textarea
+            name="history-comment"
+            onChange={(e) => {
+              setHistoryInput({ ...historyInput, comment: e.target.value });
+            }}
+            type="text"
+          />
+        </div>
       </div>
       <div className="btn-container">
-        <Button className="btn-item" variant="contained" endIcon={<SendIcon />} onClick={onClickSaveHistory}>
+        <Button
+          className="btn-item"
+          variant="contained"
+          endIcon={<SendIcon />}
+          onClick={onClickSaveHistory}
+        >
           Save History
         </Button>
 
