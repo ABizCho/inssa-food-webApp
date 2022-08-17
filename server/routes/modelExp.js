3de657fd4845bdf51efd2d63198b09b2fc0cdf37
horz.d <he1236@ajou.ac.kr>
2022-08-18 04:13:53 +0900

    server up

--- server/routes/modelExp.js
+++ server/routes/modelExp.js
@@ -5,15 +5,13 @@ const router = Router();
 const portUrl = require("../portUrl");
 router.get("/", async (req, res, next) => {
   const FLASK_SERVER = portUrl.cloudServer + portUrl.flaskPort;
-  const imgUrl = req.body;
-  console.log("server test:", imgUrl);
+  const { cookieImg } = req.body;
+  console.log("server test:", cookieImg);
 
   try {
-    await axios
-      .get(FLASK_SERVER + `/modelCall/${imgUrl.filename}`)
-      .then((res) => {
-        console.log(res);
-      });
+    await axios.get(FLASK_SERVER + `/modelCall/${imgUrl}`).then((res) => {
+      console.log(res);
+    });
     // await axios.get(FLASK_SERVER + `/modelCall`).then((res) => {
     //   console.log(res);
     // });
