86465027f81e7c1bcc4d3c7ffca68fada4e692d1
horz.d <he1236@ajou.ac.kr>
2022-08-18 02:32:59 +0900

    test-3 max처리

--- flaskServer/model.py
+++ flaskServer/model.py
@@ -34,8 +34,10 @@ def modelRun(imgUrl) :
     
     print(features) 
     print(type(features))
-    print(features[0])
+    tmp = max(features[0])
+    index = features[0].index(tmp)
+    print(index)
     # plt.imshow(features[0,:,:,3]) # 4번째 피쳐맵(특징)을 그려보자. cf. 0부터 시작하기에 4번째임.
-    return features[0]
+    return index
 
 modelRun('/root/inssa-food/flaskServer/img_image21660548025317.jpg')
