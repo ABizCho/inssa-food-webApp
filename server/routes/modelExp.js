f89b2921ebaf22ebd909f45fca5799489c4426b0
horz.d <he1236@ajou.ac.kr>
2022-08-29 02:14:40 +0900

    직전 merge 충돌났던 부분 (modelExp) 충돌해결 후 push

--- server/routes/modelExp.js
+++ server/routes/modelExp.js
@@ -3,24 +3,11 @@ const axios = require("axios");
 
 const router = Router();
 const portUrl = require("../portUrl");
-<<<<<<< HEAD
-router.get("/", async (req, res, next) => {
-  const FLASK_SERVER = portUrl.cloudServer + portUrl.flaskPort;
-
-  imgUrl = req.body;
-
-  try {
-    await axios.get(FLASK_SERVER + `/modelCall/${imgUrl}`).then((res) => {
-      console.log(res);
-    });
-  } catch (e) {
-=======
 
 router.get("/uploads/:imgUrl", async (req, res, next) => {
   const FLASK_SERVER = portUrl.cloudServer + portUrl.flaskPort;
   const { imgUrl } = req.params;
   console.log("server test:", imgUrl);
-
 
   let modelResult;
 
@@ -31,12 +18,11 @@ router.get("/uploads/:imgUrl", async (req, res, next) => {
       modelResult = res.data;
     });
     // await axios.get(FLASK_SERVER + `/modelCall`).then((res) => {
-      // });
-      res.json(modelResult);
+    // });
+    res.json(modelResult);
   } catch (e) {
     console.log(e);
 
->>>>>>> 37eab0ef3a1837b8a8c759d3109f5297153a6141
     next(e);
   }
 });
