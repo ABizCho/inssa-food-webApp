62617520039ead99a9f7de1ff19c74335ae944ba
hongregii <98505239+hongregii@users.noreply.github.com>
2022-08-29 01:18:26 +0900

    rest_API_Key, redir URI 수정

--- client/src/pages/user/Login.js
+++ client/src/pages/user/Login.js
@@ -15,7 +15,7 @@ const Login = () => {
   const navigate = useNavigate();
   // ------------------kakao Oauth-------------------
 
-  const REST_API_KEY = "0abf97780f442400eccc7cd004baabab";
+  const REST_API_KEY = "25fbc98ddab2446ddeb681e7af004a9e";
   const REDIRECT_URI = "http://115.85.182.215/oauth/kakao/callback";
 
   //1번
