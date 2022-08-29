0dea7c3448b05d2d81877b6ff543cb8de7aed7a8
hongregii <98505239+hongregii@users.noreply.github.com>
2022-08-29 21:33:20 +0900

    About page (#58)
    * about page
    
    1
    
    * 3people
    
    profile
    
    Co-authored-by: Ckck12 <100400mjkim@gmail.com>

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -1,18 +1,85 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebookF } from "@fortawesome/free-brands-svg-icons";
 import Chan_profile from "./chan.jpg";
+import Hong_profile from "./Hong.jpg";
 import "./About.css";
+
+import { Link } from "react-router-dom";
 
 const About = () => {
   const navigate = useNavigate();
-  library.add(faFacebookF);
+
+  const ButtonMailto = ({ mailto, label }) => {
+    return (
+      <Link
+        to="#"
+        onClick={(e) => {
+          window.location.href = mailto;
+          e.preventDefault();
+        }}
+      >
+        {label}
+      </Link>
+    );
+  };
 
   return (
     <div>
+      <div className="profile_container">
+        <div className="cover-photo">
+          <img src={Hong_profile} className="profile" />
+        </div>
+        <div className="profile-name">Hongrai Kim</div>
+        <p className="about">
+          Team Leader / Front-end
+          <br />
+          Back-end / Deep Learning
+        </p>
+        <ButtonMailto className="msg-btn" label="E-Mail" mailto="mailto:hongregii2@gmail.com" />
+
+        <button
+          className="follow-btn"
+          onClick={() => {
+            window.open("https://github.com/hongregii");
+          }}
+          alt=""
+          src="https://github.com/hongregii"
+        >
+          Github
+        </button>
+      </div>
+
+      <div className="profile_container">
+        <div className="cover-photo">
+          <img src={Chan_profile} className="profile" />
+        </div>
+
+        <div className="profile-name">Seongwoo Cho</div>
+
+        <p className="about">
+          Full-Stack Dev
+          <br />
+          Front-end develop
+        </p>
+
+        <button className="msg-btn">Message</button>
+        <button
+          className="follow-btn"
+          onClick={() => {
+            window.open("https://github.com/Ckck12");
+          }}
+          alt=""
+          src="https://github.com/Ckck12"
+        >
+          Github
+        </button>
+      </div>
+
       <div className="profile_container">
         <div className="cover-photo">
           <img src={Chan_profile} className="profile" />
@@ -27,13 +94,16 @@ const About = () => {
         </p>
 
         <button className="msg-btn">Message</button>
-        <button className="follow-btn">Following</button>
-
-        <div>
-          <i className="fab fa-instagram"></i>
-          <i className="fab fa-youtube"></i>
-          <i className="fab fa-twitter"></i>
-        </div>
+        <button
+          className="follow-btn"
+          onClick={() => {
+            window.open("https://github.com/Ckck12");
+          }}
+          alt=""
+          src="https://github.com/Ckck12"
+        >
+          Github
+        </button>
       </div>
     </div>
   );
